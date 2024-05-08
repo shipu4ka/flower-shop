eb6db7aa7ecc3fb989f933df961bf8c5f9f1f8c0
Tatiana Shipulina <grafit.1@yandex.ru>
2024-05-08 18:01:29 +0300

    feat: add support react

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+export const App: FunctionComponent = () => {
+  return <div>My flower shop</div>;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   module: {
     rules: [
       { test: /\.svg$/, use: "svg-inline-loader" },
@@ -16,10 +18,22 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: 'assets',
+        },
+      ],
     }),
   ],
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
